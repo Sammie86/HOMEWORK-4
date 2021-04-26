3dc1ac4c2782beff93df6f3dc111a2a172a79797
Sammie86 <77919972+Sammie86@users.noreply.github.com>
2021-04-26 09:59:39 -0500

    Add files via upload

---
+++ 129Exceptionhandling.py
@@ -0,0 +1,13 @@
+print('Iyai Bassey 1937369') # This is my name and PSID
+result = []
+
+while(1):
+    string = input()
+    if(string == "-1"):
+        break
+    try:
+        result.append([string.split(' ')[0],int(string.split(' ')[1])+1])
+    except ValueError:
+        result.append([string.split(' ')[0],0])
+for name, age in result:
+    print(name,age)
